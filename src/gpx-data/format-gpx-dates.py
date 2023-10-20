1b5464f370e8e8c9bb4fc9a076259dbb2635ea99
Duckiduc <git@ducki.dev>
2023-10-21 01:21:20 +0200

    Add args

--- src/gpx-data/format-gpx-dates.py
+++ src/gpx-data/format-gpx-dates.py
@@ -1,36 +1,47 @@
 import re
 import gpxpy
 from datetime import datetime
+import argparse
 
-# Open the GPX file
-with open("file.gpx", "r") as f:
-    gpx = gpxpy.parse(f)
 
-# Define the regex pattern to match the time value
-pattern = r"<time>(.+)</time>"
+def update_gpx_time_format(input_file):
+    # Open the GPX file
+    with open(input_file, "r") as f:
+        gpx = gpxpy.parse(f)
 
-# Loop through the track points and update the time format
-for track in gpx.tracks:
-    for segment in track.segments:
-        for point in segment.points:
-            # Extract the time value using regex
-            match = re.search(pattern, str(point))
-            if match:
-                time_str = match.group(1)
-                time_obj = datetime.strptime(time_str, "%m/%d/%YT%H:%M:%SZ")
+    # Define the regex pattern to match the time value
+    pattern = r"<time>(.+)</time>"
 
-                # Format the time in the desired format
-                time_formatted = time_obj.strftime("%Y:%m:%d %H:%M:%S %z")
+    # Loop through the track points and update the time format
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                # Extract the time value using regex
+                match = re.search(pattern, str(point))
+                if match:
+                    time_str = match.group(1)
+                    time_obj = datetime.strptime(time_str, "%m/%d/%YT%H:%M:%SZ")
 
-                # Add a new element for the updated time value
-                point.extensions.append("<time>{}</time>".format(time_formatted))
+                    # Format the time in the desired format
+                    time_formatted = time_obj.strftime("%Y:%m:%d %H:%M:%S %z")
 
-                # Remove the original time element
-                for ext in point.extensions:
-                    if "<time>" in ext:
-                        point.extensions.remove(ext)
-                        break
+                    # Add a new element for the updated time value
+                    point.extensions.append("<time>{}</time>".format(time_formatted))
 
-# Save the updated GPX file
-with open("output_file.gpx", "w") as f:
-    f.write(gpx.to_xml())
+                    # Remove the original time element
+                    for ext in point.extensions:
+                        if "<time>" in ext:
+                            point.extensions.remove(ext)
+                            break
+
+    # Save the updated GPX file
+    with open("output_file.gpx", "w") as f:
+        f.write(gpx.to_xml())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Update GPX file time format")
+    parser.add_argument("input_file", help="Input GPX file path")
+
+    args = parser.parse_args()
+    update_gpx_time_format(args.input_file)
